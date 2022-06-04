9f23cb4db99aee4519e012b718cd780e35a3f5fc
relibrana <relibrana@outlook.com>
2022-06-04 15:24:02 -0500

    ProjectCreation

---
+++ src/main/java/com/finanzas/trabajofinal/TrabajoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.finanzas.trabajofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoFinalApplication.class, args);
+    }
+
+}
